59ed8f5789cb9fdc049f1f1f5f3f2b184a422189
PechMV <Pechm607@gmail.com>
2022-10-17 20:51:14 -0500

    added dropdown box in css and changed js file for search results

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// 
+var select = document.getElementById('myCities');
+
 // Initialize and add the map
-function initMap() {
+function initMap(restaurants) {
   // The location of Uluru
-  console.log("hi")
   const uluru = { lat: 44.9778, lng: -93.2650 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -17,11 +19,26 @@ function initMap() {
 
 window.initMap = initMap;
 
+// var city = "";
+var minneapolis = "43323";
+var duluth = "43018";
+var stPaul = "43501";
+var minnetonka = "43327";
+var eagan = "43022";
+var bloomington = "36942";
 
+function restaurantFetch(event){
+console.log ("hi")
+var city = select.options[select.selectedIndex].value;
+
+// if stPaul clicked then city = loc id. var city = ()
+// click event 
+
+// Minneapolis
 const encodedParams = new URLSearchParams();
 encodedParams.append("language", "en_US");
 encodedParams.append("limit", "30");
-encodedParams.append("location_id", "55414");
+encodedParams.append("location_id", city);
 encodedParams.append("currency", "USD");
 
 const options = {
@@ -36,25 +53,60 @@ const options = {
 
 fetch('https://worldwide-restaurants.p.rapidapi.com/search', options)
   .then(response => response.json())
-  .then(response => console.log(response))
+  .then(response => {
+    var restaurants = response.results.data;
+    var displayBox = document.getElementById("container");
+    for(var i = 0; i < restaurants.length; i++){
+      var li = document.createElement("li");
+      li.textContent = restaurants[i].name;
+      displayBox.appendChild(li)
+    }
+   console.log(response)
+   initMap(restaurants)
+  })
   .catch(err => console.error(err));
 
+}
+
+
+  //drop down
+//   var myOptions = {
+//     val1 : 'bloomington',
+//     val2 : 'duluth',
+//     val3 : 'eagan',
+//     val4 : 'minneapolis',
+//     val5 : 'minnetonka',
+//     val6 : 'stPaul'
+// };
+// var mySelect = $('#myCities');
+// $.each(myOptions, function(val, text) {
+//     mySelect.append(
+//         $('<option></option>').val(val).html(text)
+//     );
+// });
+
 // jQuery
-$('#submitid').click(function () {
-  var query = $('#myid').val();
-  var apiKey = 'AIzaSyDrvgCJGPsMiJTXsoFfwRcpGKqxP30P_jk';
-  var baseUrl = '';
+// // $('#submitid').click(function () {
+//   var query = $('#myid').val();
+//   var apiKey = 'AIzaSyDrvgCJGPsMiJTXsoFfwRcpGKqxP30P_jk';
+//   var baseUrl = 'https://maps.googleapis.com/';
 
-  var foodSearchUrl = baseUrl + ''
+//   var foodSearchUrl = baseUrl + 'maps/api/js?key=' + apiKey;
 
-$(document).ready(function() {
-  $("#someform").submit(function(){
-      $.ajax({
-          url: foodSearchUrl + '&q=' + encodeURI(query),
-          dataType: "jsonp",
-          success: searchCallback
-      });
-      return false;
-  });
-});
+// $(document).ready(function() {
+//   $("#someform").submit(function(){
+//       $.ajax({
+//           url: foodSearchUrl + '&q=' + encodeURI(query),
+//           dataType: "jsonp",
+//           success: searchCallback
+//       });
+//       return false;
+//   });
+// });
 
+//   function searchCallback(data) {
+//     $(document.body).append(
+//       'Found ' + data.total + ' results for ' + query
+//   );
+//     var city = data.city;
+//     $.each(city, function (index, city));
